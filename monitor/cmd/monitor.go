49d14f359d9c3e62e1210b097f76ae2969e60e52
JavaHutt <alxmer111@gmail.com>
2021-02-14 23:35:05 +0400

    graceful shutdown

--- monitor/cmd/monitor.go
+++ monitor/cmd/monitor.go
@@ -6,8 +6,12 @@ import (
 	"monitor/internal/adaptor"
 	"monitor/internal/rmq"
 	"monitor/internal/service"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/viper"
+	"github.com/streadway/amqp"
 )
 
 func initConfig() error {
@@ -57,29 +61,42 @@ func main() {
 		log.Fatal("Failed to declare a queue ", err)
 	}
 
-	msgs, err := ch.Consume(q.Name, "", false, false, false, false, nil)
+	msgs, err := ch.Consume(q.Name, "consumer", false, false, false, false, nil)
 	if err != nil {
 		log.Fatal("Failed to register a consumer ", err)
 	}
 
-	forever := make(chan bool)
+	forever := make(chan os.Signal)
+	signal.Notify(forever, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		for d := range msgs {
-			parsed, err := service.ParseAlarm(d.Body)
-			if err != nil {
-				log.Fatal("Failed parse incoming alarm ", err)
-			}
+	done := make(chan bool)
 
-			err = service.InsertAlarm(ctx, *parsed)
-			if err != nil {
-				log.Fatal("Failed insert incoming alarm ", err)
-			}
-
-			d.Ack(false)
-		}
-	}()
+	go recieveAlarms(ctx, service, msgs, done)
 
 	log.Printf("Waiting for messages. To exit press CTRL+C")
 	<-forever
+
+	ch.Cancel("consumer", false)
+	log.Println("Stopped receiving message from queue")
+	log.Println("Wait for worker process recieved message")
+	<-done
+	log.Println("Woker done")
+}
+
+func recieveAlarms(ctx context.Context, service *service.Service, msgs <-chan amqp.Delivery, done chan bool) {
+	for m := range msgs {
+		parsed, err := service.ParseAlarm(m.Body)
+		if err != nil {
+			log.Fatal("Failed parse incoming alarm ", err)
+		}
+
+		err = service.InsertAlarm(ctx, *parsed)
+		if err != nil {
+			log.Fatal("Failed insert incoming alarm ", err)
+		}
+
+		m.Ack(false)
+		log.Println("Alarm acked")
+	}
+	done <- true
 }
